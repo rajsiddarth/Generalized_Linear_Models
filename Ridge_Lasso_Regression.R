d0c1c4c3b0e39992dec4cbc45426a6a709b370f1
Siddarth <rajasiddartha94@gmail.com>
2017-08-17 01:13:55 -0700

    Update Ridge_Lasso_Regression.R

--- Ridge_Lasso_Regression.R
+++ Ridge_Lasso_Regression.R
@@ -2,7 +2,7 @@ rm(list = ls(all=TRUE))
 
 #Loading data
 library(RCurl)
-data=read.csv("https://raw.githubusercontent.com/rajsiddarth119/
+data=read.csv("https://raw.githubusercontent.com/rajsiddarth/
                    Generalized_Linear_Models/master/CustomerData.csv",header=T)
 #Checking data
 str(data)
